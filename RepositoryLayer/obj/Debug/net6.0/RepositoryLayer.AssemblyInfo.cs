0da24e70b8fc6934b889dc528ccfe7787ad48a8c
Sahil <sahilpandey412@gmail.com>
2025-03-22 10:05:17 -0600

    UC1

--- RepositoryLayer/obj/Debug/net6.0/RepositoryLayer.AssemblyInfo.cs
+++ RepositoryLayer/obj/Debug/net6.0/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13ac187a413a32d263b732998bacd9423dc2dd78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e79c66222c15174b193158b6dcde936436d4b258")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
