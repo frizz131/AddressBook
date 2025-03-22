13ac187a413a32d263b732998bacd9423dc2dd78
Sahil <sahilpandey412@gmail.com>
2025-03-22 09:33:31 -0600

    UC1

---
+++ AddressBook/Controllers/AddressBookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.AddressBookDbContext;
+
+namespace AddressBook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressBookController : ControllerBase
+    {
+        private readonly AddressBookContext _context;
+
+        public AddressBookController(AddressBookContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetContacts()
+        {
+            var contacts = _context.Contacts.ToList();
+            return Ok(contacts);
+        }
+    }
+}
---
+++ RepositoryLayer/AddressBookDbContext/AddressBookContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Models;
+
+namespace RepositoryLayer.AddressBookDbContext
+{
+    public class AddressBookContext : DbContext
+    {
+        public AddressBookContext(DbContextOptions<AddressBookContext> options)
+            : base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
